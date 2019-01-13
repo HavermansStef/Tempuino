b1e8e08a96fe2b8b3b71482bccb44dfecb767ce4
Stef Havermans <stef.havermans@student.kdg.be>
2019-01-13 22:45:57 +0100

    Last few bits

--- Tempuino/Arduino Files/Tempuino/Tempuino.ino
+++ Tempuino/Arduino Files/Tempuino/Tempuino.ino
@@ -31,6 +31,7 @@ float temp; //Stores temperature value
 int tempDelay = 10000;
 
 
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 WiFiClient espClient;
 PubSubClient client(espClient);
